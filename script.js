3e738964f76468900a4c31419cfcf0056f06cded
wandersonwendel <wandsolopes@alu.ufc.br>
2024-01-27 09:55:23 -0300

    Cria a lógica de saudação e ajustes dos cards - Implemta a lógica de que texto aparecerá de boas-vindas; - Novo código no script.js, criando uma forma de mostrar na tela mensagem de Bom dia, Boa tarde ou Boa noite; - Atualização do container dos cards.

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-/* Declarando as variáveis */
-const searchInput = document.getElementById("search-input");
-const resultsArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById("result-playlists")
+//BOM DIA | BOA TARDE | BOA NOITE
 
-function requestApi(searchTerm){
-    /* Consumindo a API */
-    /* Passando uma url da api que contém todas as informações dos artistas filtrando (?name_like=..) pela var searchTerm, buscando os artistas cujos nomes se assemelham ao termo de pesquisa fornecido. */
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        /* Utliza then para lidar com essa operação de busca nessa url, com essa promisse retornada em feth*/
-        .then((response) => response.json()) /* Recebe a resposta da solicitação da url, em seu argumento response e converte para objeto json*/
-        .then((result) => displayResults(result)) /* Recebe essa resposta transformada em json, em seu argumento result */
-}
+// Obtém a referência do elemento com o ID "greeting"
+const greetingElement = document.getElementById("greeting");
 
-function displayResults(result){
-    resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+// Obtém a hora atual do sistema
+const currentHour = new Date().getHours();
 
-    result.forEach(element => { /* Pegando cada elemento, onde cada um será um item do result*/
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
+// Define a saudação com base na hora atual
+// if (currentHour >= 5 && currentHour < 12) {
+//   greetingElement.textContent = "Bom dia";
+// } else if (currentHour >= 12 && currentHour < 18) {
+//   greetingElement.textContent = "Boa tarde";
+// } else {
+//   greetingElement.textContent = "Boa noite";
+// }
 
-    resultsArtist.classList.remove('hidden');
-}
+// Forma mais simples
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
 
+greetingElement.textContent = greetingMessage;
 
+// GRID INTELIGENTE
+const container = document.querySelector(".offer__list-item");
 
+const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
+  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
 
-/* Manipulação de eventos
+  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 
-"Ouvindo", se algo ocorrer no input, faça o que tiver na função */
-document.addEventListener("input", function() {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === ''){ /* Quando nada for digitado */
-        resultPlaylist.classList.remove("hidden"); /* Remove classe hidden, já tem estilo de display none, para esconder o componente com ID result-playlists da variável resultPlylist */
-        resultsArtist.classList.add("hidden"); /* Adiciona a classe hidden que já tem de primeira no componente de ID result-artist da variável resultArtists */
-        return; /* Só para parar a execução pq não precisa que nada aconteça se n tem nada digitado*/
-    }
-
-    requestApi(searchTerm);
+  console.log({ container });
+  console.log({ numColumns });
 });
+//observando a mudança do elemento
+observer.observe(container);
